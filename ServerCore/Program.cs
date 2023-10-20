ce689a3c68c3e69582d9e3cf75110a0ffa56686c
강형규 <@HGKANG>
2023-10-20 16:46:34 +0900

    초기 커밋.

---
+++ ServerCore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static ThreadLocal<string> threadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        static void WhoAml()
+        {
+            //threadName.Value = 
+            bool repeat = threadName.IsValueCreated;
+            if(repeat)
+                Console.WriteLine(threadName.Value + "(repeat)");
+            else
+                Console.WriteLine(threadName.Value );
+
+            //Thread.Sleep(1000);
+
+        }
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            //쓰레드 풀에서 사용 
+            Parallel.Invoke(WhoAml, WhoAml, WhoAml, WhoAml, WhoAml, WhoAml, WhoAml, WhoAml);
+        }
+    }
+}
